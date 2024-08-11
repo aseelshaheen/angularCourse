4b4564fe862d5778890b28734f7e0dda12229d82
aseelshaheen <aseelshaheen080@gmail.com>
2024-08-11 15:36:01 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from "./top-header/top-header.component";
+import { TopMenuComponent } from "./header/top-menu/top-menu.component";
+import { MainMenuComponent } from "./header/main-menu/main-menu.component"; // Import HeaderComponent
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [HeaderComponent, TopHeaderComponent, TopMenuComponent, MainMenuComponent] // Add HeaderComponent to the imports
+ // Add HeaderComponent to the imports
+ // Add HeaderComponent to the imports
+ // Add HeaderComponent to the imports
 })
 export class AppComponent {
   title = 'angularCourse';
---
+++ src/app/header/main-menu/main-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-menu',
+  standalone: true,
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.css'] // Ensure styleUrls is correctly spelled
+})
+export class MainMenuComponent { }
